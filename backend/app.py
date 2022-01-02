efc5149c6f35524e39a2345fd450c2d4894eb1d9
Lucas <lucaspingpao@gmail.com>
2022-01-03 00:19:31 +0100

    heroku deploy try 1

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,23 @@
 from flask import Flask, jsonify
-from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
+from flask.helpers import send_from_directory
+# from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='frontend/build', static_url_path='')
+CORS(app)
 
-app.config['SQLACHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/flask'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'mysql://root:''@localhost/flask'
+# app.config['SQLACHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/flask'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'mysql://root:''@localhost/flask'
 
-@app.route('/get', methods=['GET'])
-def get_requests():
-    return
+@app.route('/api', methods=['GET'])
+@cross_origin()
+def testing():
+    return {
+        "testing the api works here ✅": "Heroku"
+    }
+
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
+
+if __name__ == '__main__':
+    app.run()
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,13 +6,29 @@ import Login from './components/app_components/Login';
 import Signup from './components/app_components/Signup';
 import GlobalControls from './components/app_components/GlobalControls';
 
+import { useState, useEffect } from 'react';
+
 function App() {
+
+  // test the Flask API here
+  const [testapi, setTestapi] = useState({});
+  useEffect(() => {
+    fetch('/api').then(response => {
+      if (response.status == 200) {
+        return response.json()
+      }
+    }).then(data => setTestapi(data))
+    .then(error => console.log(error))
+  }, [])
+
+  // return different comopnents
   return (
     <BrowserRouter>
     <div className="App">
       <Routes>
         <Route path='/' element={
           <div>
+            <div className='apitest'>{Object.keys(testapi)}</div>
             <Navbar/><Sidebar/><GlobalControls/>
           </div>
         }/>
