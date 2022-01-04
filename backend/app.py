3ecf6b3b4192e2c91b6bfd2c6da077c36a2af0a7
Lucas <lucaspingpao@gmail.com>
2022-01-04 23:56:08 +0100

    deploy to heroku

--- backend/app.py
+++ backend/app.py
@@ -1,22 +1,29 @@
-from flask import Flask, request
+from flask import Flask, request, send_from_directory
 from flask_cors import CORS, cross_origin
 # from flask_sqlalchemy import SQLAlchemy
 import json
 from helpers import *
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../frontend/build', static_url_path='')
 cors = CORS(app)
 
 # app.config['SQLACHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/flask'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'mysql://root:''@localhost/flask'
 
+@app.route('/')
+#@cross_origin
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
+
 @app.route('/api', methods=['GET'])
+#@cross_origin
 def testing():
     return {
         "testing the api works here ✅": "Heroku"
     }
 
 @app.route('/rhythm', methods=['POST'])
+#@cross_origin
 def rhythm():
     data = json.loads(request.get_data().decode())
     tracks = {}
@@ -26,45 +33,20 @@ def rhythm():
     return request.data
 
 @app.route('/harmony', methods=['POST'])
+#@cross_origin
 def harmony():
     data = json.loads(request.get_data().decode())
     mix_chords(data, 1000)
     return request.data
 
 @app.route('/play', methods=['POST'])
+#@cross_origin
 def global_play():
-    drums = AudioSegment.from_wav('/Users/lucaspao/Documents/Coding/tap-jams/frontend/src/assets/sounds/test_drums.wav')
-    chords = AudioSegment.from_wav('/Users/lucaspao/Documents/Coding/tap-jams/frontend/src/assets/sounds/test_piano.wav')
+    drums = AudioSegment.from_wav('../frontend/src/assets/sounds/test_drums.wav')
+    chords = AudioSegment.from_wav('../frontend/src/assets/sounds/test_piano.wav')
     song = drums.overlay(chords)
     play(song)
     return request.data
 
 if __name__ == '__main__':
     app.run()
-
-
-'''
-from flask import Flask, jsonify
-from flask_cors import CORS, cross_origin
-from flask.helpers import send_from_directory
-from flask_sqlalchemy import SQLAlchemy
-
-app = Flask(__name__, static_folder='frontend/build', static_url_path='')
-CORS(app)
-
-# app.config['SQLACHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/flask'
-# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'mysql://root:''@localhost/flask'
-
-@app.route('/api', methods=['GET'])
-@cross_origin()
-def testing():
-    return {
-        "testing the api works here ✅": "Heroku"
-    }
-
-def serve():
-    return send_from_directory(app.static_folder, 'index.html')
-
-if __name__ == '__main__':
-    app.run()
-'''
--- backend/helpers.py
+++ backend/helpers.py
@@ -3,10 +3,10 @@ from scipy.io import wavfile
 from pydub.playback import play # Used for playing sound in Python
 
 sample_song = {
-    '/Users/lucaspao/Documents/Coding/tap-jams/frontend/src/assets/sounds/Cymbal/Boom.wav': '1000000000000000',
-    '/Users/lucaspao/Documents/Coding/tap-jams/frontend/src/assets/sounds/Hi-Hats/ClosedHat.wav': '1111111111111111',
-    '/Users/lucaspao/Documents/Coding/tap-jams/frontend/src/assets/sounds/Kick/Kick.wav': '1000000010000000',
-    '/Users/lucaspao/Documents/Coding/tap-jams/frontend/src/assets/sounds/Snare/Snare.wav': '0000100000001000',
+    '../frontend/src/assets/sounds/Cymbal/Boom.wav': '1000000000000000',
+    '../frontend/src/assets/sounds/Hi-Hats/ClosedHat.wav': '1111111111111111',
+    '../frontend/src/assets/sounds/Kick/Kick.wav': '1000000010000000',
+    '../frontend/src/assets/sounds/Snare/Snare.wav': '0000100000001000',
 }
 
 def mix_tracks(sounds, beat_time):
@@ -23,22 +23,22 @@ def mix_drums(song_data, beat_time):
     for beat in range(16):
         processing_sounds = [AudioSegment.from_wav(i) for i in song_data.keys() if song_data[i][beat] == '1']
         if processing_sounds:
-            next = mix_tracks(processing_sounds, beat_time)
-            print(next.duration_seconds)
+            # next = mix_tracks(processing_sounds, beat_time)
+            # print(next.duration_seconds)
             song += mix_tracks(processing_sounds, beat_time)
         else:
             song += AudioSegment.silent(beat_time)
     song += AudioSegment.silent(beat_time)
     play(song)
-    song.export('/Users/lucaspao/Documents/Coding/tap-jams/frontend/src/assets/sounds/test_drums.wav', format='wav')
+    song.export('../frontend/src/assets/sounds/test_drums.wav', format='wav')
     return song
 
 def mix_chords(chords, measure_time):
     song = sum([AudioSegment.from_wav('../frontend/src/assets/sounds/Piano Chords/' + c + '.wav')[:measure_time] for c in chords])
     play(song)
-    song.export('/Users/lucaspao/Documents/Coding/tap-jams/frontend/src/assets/sounds/test_piano.wav', format='wav')
+    song.export('../frontend/src/assets/sounds/test_piano.wav', format='wav')
     return song
 
 #song = mix_drums(sample_song, 250)
 #play(song)
-#song.export('/Users/lucaspao/Documents/Coding/tap-jams/frontend/src/assets/sounds/test_mix.wav', format='wav')
+#song.export('../frontend/src/assets/sounds/test_mix.wav', format='wav')
