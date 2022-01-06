f4d8ae415d5c606a070ba1d0e20e56fd30934188
Lucas <lucaspingpao@gmail.com>
2022-01-06 03:06:09 +0100

    fix heroku

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
-from flask import Flask, request, send_from_directory
+from flask import Flask, request
+from flask.helpers import send_from_directory
 from flask_cors import CORS, cross_origin
 # from flask_sqlalchemy import SQLAlchemy
-import json
+# import json
 # from backend.helpers import *
 
 app = Flask(__name__, static_folder='frontend/build', static_url_path='')
-#cors = CORS(app)
+CORS(app)
 
 # app.config['SQLACHEMY_DATABASE_URI'] = 'mysql://root:''@localhost/flask'
 # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = 'mysql://root:''@localhost/flask'
 
-@app.route('/')
-#@cross_origin
-def serve():
-    return send_from_directory(app.static_folder, 'index.html')
-
 @app.route('/api', methods=['GET'])
-#@cross_origin
+@cross_origin
 def testing():
     return {
         "testing the api works here": "Heroku"
     }
+
+@app.route('/')
+@cross_origin
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
 
 # @app.route('/rhythm', methods=['POST'])
 # #@cross_origin
