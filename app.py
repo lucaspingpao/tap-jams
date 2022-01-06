0319c34ec4a19bcdae9e1ec0374c84825a8c128c
Lucas <lucaspingpao@gmail.com>
2022-01-06 02:57:10 +0100

    testing heroku

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, send_from_directory
 from flask_cors import CORS, cross_origin
 # from flask_sqlalchemy import SQLAlchemy
 import json
-from backend.helpers import *
+# from backend.helpers import *
 
 app = Flask(__name__, static_folder='frontend/build', static_url_path='')
 #cors = CORS(app)
@@ -22,31 +22,31 @@ def testing():
         "testing the api works here": "Heroku"
     }
 
-@app.route('/rhythm', methods=['POST'])
-#@cross_origin
-def rhythm():
-    data = json.loads(request.get_data().decode())
-    tracks = {}
-    for instrument in data['sounds'].keys():
-        tracks['../frontend/src/assets/sounds/' + instrument + '/' + data['sounds'][instrument]] = data['filled'][instrument]
-    mix_drums(tracks, 250)
-    return request.data
+# @app.route('/rhythm', methods=['POST'])
+# #@cross_origin
+# def rhythm():
+#     data = json.loads(request.get_data().decode())
+#     tracks = {}
+#     for instrument in data['sounds'].keys():
+#         tracks['../frontend/src/assets/sounds/' + instrument + '/' + data['sounds'][instrument]] = data['filled'][instrument]
+#     mix_drums(tracks, 250)
+#     return request.data
 
-@app.route('/harmony', methods=['POST'])
-#@cross_origin
-def harmony():
-    data = json.loads(request.get_data().decode())
-    mix_chords(data, 1000)
-    return request.data
+# @app.route('/harmony', methods=['POST'])
+# #@cross_origin
+# def harmony():
+#     data = json.loads(request.get_data().decode())
+#     mix_chords(data, 1000)
+#     return request.data
 
-@app.route('/play', methods=['POST'])
-#@cross_origin
-def global_play():
-    drums = AudioSegment.from_wav('../frontend/src/assets/sounds/test_drums.wav')
-    chords = AudioSegment.from_wav('../frontend/src/assets/sounds/test_piano.wav')
-    song = drums.overlay(chords)
-    play(song)
-    return request.data
+# @app.route('/play', methods=['POST'])
+# #@cross_origin
+# def global_play():
+#     drums = AudioSegment.from_wav('../frontend/src/assets/sounds/test_drums.wav')
+#     chords = AudioSegment.from_wav('../frontend/src/assets/sounds/test_piano.wav')
+#     song = drums.overlay(chords)
+#     play(song)
+#     return request.data
 
 if __name__ == '__main__':
     app.run()
